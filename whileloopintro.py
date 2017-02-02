c7b4469095e9118eb4aa292dbc5a882d818ec388
Sreejith <unda@Sreejiths-MacBook-Pro.local>
2017-02-02 23:28:16 +0530

    while

---
+++ whileloopintro.py
@@ -0,0 +1,12 @@
+counter = 0
+while counter<5 :
+	print("Hello "+ str(counter))
+	counter = counter + 1
+
+
+counter = 0
+while True :
+	print ("Hello " +str(counter))
+	counter = counter + 1
+	if (counter > 5):
+		break
