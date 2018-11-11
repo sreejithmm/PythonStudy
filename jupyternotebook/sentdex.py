da92efc3758b906c04ce29b3f53da96c132edf54
sreejithmm <sreejith.mm@gmail.com>
2018-11-11 17:27:03 +0530

    sentdex learning

---
+++ jupyternotebook/sentdex.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[6]:
+
+
+print ("Hello world")
+
+
+# In[7]:
+
+
+x,y = (10,9)
+print(x)
+print(y)
+
+
+# In[8]:
+
+
+condition = 1
+while condition < 10:
+    print (condition)
+    condition+=1
+
