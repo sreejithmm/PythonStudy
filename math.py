5679e5f230d2177e85579c51e93f06f351073c6b
sreejithmm <sreejith.mm@gmail.com>
2018-11-11 15:47:14 +0530

    new files

---
+++ math.py
@@ -0,0 +1,11 @@
+# + - * /
+
+'''
+hello world.. 
+this is a multiple line comment
+'''
+
+print(1+3)
+print(1*3)
+print(1/3)
+print(1-3)
---
+++ start.py
@@ -0,0 +1,5 @@
+import random
+
+print ('helloworld')
+print (random.randint(8,16))
+print ('We\'re going to the store')
